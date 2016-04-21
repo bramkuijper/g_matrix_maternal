d4c087ff298f14fc9c6126ea3d8192e663e5416e
Bram Kuijper <bramkuijper@gmail.com>
2016-04-21 15:06:03 +0100

    changes to perturbation thingies. there must be something in there

---
+++ src/run_g_fluctuate_perturb.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+./xg_m_fluctuate_perturb 0.22360679775 0.22360679775  0     0.0002 0.02 0.02    .7 .7    0   1.0 1.0     0 0     0.5 0.5  .78   0 0 1.0 0 &
+./xg_m_fluctuate_perturb 0.22360679775 0.22360679775  0     0.0002 0.02 0.02    .7 .7    0   1.0 1.0     0 0     0.5 0.5  5.5   0 0 1.0 0 &
+
+
+#    a1 = atof(argv[1]);
+#    a2 = atof(argv[2]);
+#    r_mu = atof(argv[3]);
+#    mu = atof(argv[4]);
+#    mu_m = atof(argv[5]);
+#    sdmu_m = atof(argv[6]);
+#    omega[0][0] = atof(argv[7]);
+#    omega[1][1] = atof(argv[8]);
+#    r_omega = atof(argv[9]);
+#    ampl1 = atof(argv[10]);
+#    ampl2 = atof(argv[11]);
+#    stoch1 = atof(argv[12]);
+#    stoch2 = atof(argv[13]);
+#    freq1 = atof(argv[14]);
+#    freq2 = atof(argv[15]);
+#    shift = atof(argv[16]);
+#    int1 = atof(argv[17]);
+#    int2 = atof(argv[18]);
+#    int1ptb = atof(argv[19]);
+#    int2ptb = atof(argv[20]);
